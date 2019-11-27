92565f666d7bc69222e585dfd93bdaadbeed7116
Matthias Müller <MatthiasM@Live.de>
2019-11-27 22:00:21 +0100

    WIP

--- ExpressionTesting/Program.cs
+++ ExpressionTesting/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq.Expressions;
 
 namespace ExpressionTesting
 {
@@ -6,7 +7,7 @@ namespace ExpressionTesting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
